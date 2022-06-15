ba59e9c58691911ef14a089ae0fa46d5572400fd
Grass Haus Studios <deusopus@gmail.com>
2022-06-15 06:14:51 -0700

    Create cred.py

---
+++ cred.py
@@ -0,0 +1,7 @@
+# This is your cred.py file. It contains the Twitter Developer API tokens and keys. 
+# You need to input your specific values from your Twitter Developer account settings.
+
+consumer_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+consumer_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
