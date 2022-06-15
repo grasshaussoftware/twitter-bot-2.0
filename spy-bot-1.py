9e3626f4f4a8405d9a6dbc00f2b88bfeaca446d6
Grass Haus Studios <deusopus@gmail.com>
2022-06-15 06:55:46 -0700

    Update spy-bot-1.py

--- spy-bot-1.py
+++ spy-bot-1.py
@@ -11,7 +11,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-print("Tweet-Bot v1.0 by deusopus (deusopus@gmail.com)")
+print("Tweet-Bot v2.1 by deusopus (deusopus@gmail.com)")
 print("Search criteria :", QUERY) #parses through tweets for the hashtag in config.py
 print("Follow user's accounts :", FOLLOW)
 print("Like user's tweets :", LIKE)
@@ -30,7 +30,7 @@ while True:
                 if not tweet.user.following:
                     tweet.user.follow()
                     print('Followed the user')
-                    print(tweet.text)
+
 
             # check that bot has not already favorited the tweet
             # Favorite the tweet
@@ -38,6 +38,7 @@ while True:
                 if not tweet.favorited:
                     tweet.favorite()
                     print('Liked the tweet')
+                    print(tweet.text)
 
             # Check that bot has not already retweeted the tweet
             # Retweet the tweet
@@ -49,20 +50,7 @@ while True:
             #timestamp
             time = datetime.now()
             print(time)
-
-            #delay between actions
-            print('Sleeping ', end='', flush=True)
-
-            for x in range(250):
-                for frame in r'-\|/-\|/':
-                # Back up one character then print our next frame in the animation
-                    print('\b', frame, sep='', end='', flush=True)
-                    sleep(0.2)
-
-                # Back up one character, print a space to erase the spinner, then a newline
-                # so that the prompt after the program exits isn't on the same line as our
-                # message
-            print('\b ')
+            sleep(10)
 
         except tweepy.errors.TweepyException as e:
             print(e)
