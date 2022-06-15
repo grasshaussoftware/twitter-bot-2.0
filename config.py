915b0ad738fc90a7ed65f052c74ab28ac129c6a0
Grass Haus Studios <deusopus@gmail.com>
2022-06-15 06:14:08 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+# Edit this config file as you like. Save it as 'config.py'.
+
+# This is the hashtag (can be a string) that the bot will search and follow, like, and retweet. You can edit this with any trending hashtag.
+# For example : '#javascript' or just simply 'javascript'.
+
+QUERY = 'stellarcannacoin'
+
+# Bot setting for following user who posted with your specified hashtag.
+
+FOLLOW = True
+
+# Bot setting for liking posts.
+
+LIKE = True
+
+# Bot setting to retweet posts containing the specified hashtag.
+
+RETWEET = True
+
+# Bot sleep time setting in seconds. For example 'SLEEP_TIME = 300' means 300 seconds or 5 minutes.
+# You can decrease it or increase it as you like but be careful to adhere to Twitter TOS limits.
+# Please, use large delay if you are running bot all the time so that your account does not get banned.
+
+#SLEEP_TIME = 300
